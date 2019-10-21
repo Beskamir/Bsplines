12ee5c5eaf96ec167e76ba16753e99753cad2c2f
Sebastian <sebastianvkopacz@gmail.com>
2019-10-21 13:14:52 -0600

    started on bsplines

--- src/Program.h
+++ src/Program.h
@@ -36,65 +36,21 @@ private:
 
 	void createTestGeometryObject();
 
-	// draw the cycloid 
-	void createCycloid();
-	void updateCycloid();
-
-	// draw the circles
-	void createInnerCircle();
-	void createOuterCircle();
-	void updateInnerCircle();
-	void updateOuterCircle();
-
-	// draw a dot on the last point
-	void createLastPoint();
-	void updateLastPoint();
-
-
-	void createPolynomial();
-	void updatePolynomialPoints();
-	void updatePolynomialLines();
-	// PI constant since I'm too lazy to use a library when I can just copy paste
-	float PI = 3.14159265358979323846264338327950288;
 	
 	// Class variables for controlling the hypocycloid.
-	float outerRadius = 4;
-	float innerRadius = 1;
-	int cycles = 1;
 	float rotation = 0;
 	float scale = 1;
-	int amount = 1;
-	int step = 100;
-	int circleDetail = 100;
-	bool hideInnerCircle = false;
-	bool hideOuterCircle = false;
-	bool hideDot = false;
-	bool enablePoints = false;
-
-	bool pauseAnimation = false;
-
-	bool viewHypocycloid = true;
-
-	float theta = 0;
-	float thetaCi = 0;
-	float thetaCo = 0;
-
-	bool parametersChanged = true;
+	int step = 1;
 
 	// Class variable for the hypocycloid
-	Geometry* hypocycloid;
-	Geometry* innerCircle;
-	Geometry* outerCircle;
-	Geometry* lastPoint;
-	Geometry* polynomialPoints;
-	Geometry* polynomialLine;
+	// Geometry* hypocycloid;
+	// Geometry* innerCircle;
+	// Geometry* outerCircle;
+	// Geometry* lastPoint;
+	// Geometry* polynomialPoints;
+	// Geometry* polynomialLine;
 
 	glm::vec3 *mousePosition;
-
-	float polynomialScale = 1;
-	bool applyPolynomialScale = false;
-
-	float offset[2] = { 0, 0 };
 
 	ImVec4 lineColor;
 };
